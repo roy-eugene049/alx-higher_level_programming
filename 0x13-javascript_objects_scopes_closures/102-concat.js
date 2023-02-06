0ab593f5529d39b2ba3fa0fabdbf85b83067def5
Roy Eugene <71348610+roy-eugene049@users.noreply.github.com>
2023-02-06 14:33:14 +0300

    Initial Commit

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,8 @@
-ll
+#!/usr/bin/node
+const fs = require('fs');
+const args = require('process').argv;
+
+for (let i = 2; i < args.length - 1; i++) {
+  const data = fs.readFileSync(args[i], 'utf8');
+  fs.writeFileSync(args[args.length - 1], data, { flag: 'a+' });
+}
