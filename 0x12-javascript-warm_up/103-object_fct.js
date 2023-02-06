8ec4b11b7f0c186dbf0052d1075478783b065f03
Roy Eugene <71348610+roy-eugene049@users.noreply.github.com>
2023-02-06 13:48:51 +0300

    Initial Commit

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,1 +1,17 @@
-ll
+#!/usr/bin/node
+let myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+myObject.incr = function () {
+  this.value++;
+};
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
