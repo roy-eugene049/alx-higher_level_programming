556b72a60f6c21513f499c7b3d244c5f229deea5
Roy Eugene <71348610+roy-eugene049@users.noreply.github.com>
2023-02-06 14:36:31 +0300

    Initial Commit

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,1 +1,10 @@
-ll
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let count = 0;
+  list.forEach(function (item) {
+    if (item === searchElement) {
+      count++;
+    }
+  });
+  return count;
+};
