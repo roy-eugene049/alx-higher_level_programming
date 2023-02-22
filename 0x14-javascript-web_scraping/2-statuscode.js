9e796da0856b371efbf91c0a93ccd485004cf546
Roy Eugene <71348610+roy-eugene049@users.noreply.github.com>
2023-02-22 15:46:08 +0300

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,10 @@
-ll
+#!/usr/bin/node
+const request = require('request');
+
+if (process.argv.length > 2) {
+  request
+    .get(process.argv[2])
+    .on('response', response => {
+      console.log(`code: ${response.statusCode}`);
+    });
+}
