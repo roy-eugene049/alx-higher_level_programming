ef6f355ad3cae37aa8fcc6891a41df0a644c3512
Roy Eugene <71348610+roy-eugene049@users.noreply.github.com>
2023-02-22 15:46:54 +0300

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,16 @@
-ll
+#!/usr/bin/node
+const request = require('request');
+
+if (process.argv.length > 2) {
+  request(`${process.argv[2]}`, (err, res, body) => {
+    if (err) {
+      console.log(err);
+    } else if (body) {
+      const charFilms = JSON.parse(body).results.filter(
+        x => x.characters.find(y => y.match(/\/people\/18\/?$/))
+      );
+
+      console.log(charFilms.length);
+    }
+  });
+}
