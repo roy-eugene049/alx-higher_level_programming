9ed1b499a365a6b59976772b1875a049ab305fcb
Roy Eugene <71348610+roy-eugene049@users.noreply.github.com>
2023-02-22 15:30:47 +0300

    Update 2-delete.sh

--- 0x10-pyhton-network_0/2-delete.sh
+++ 0x10-pyhton-network_0/2-delete.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# check if URL is passed as argument
-if [ -z "$1" ]
-then
-  echo "Please provide the URL as the first argument"
-  exit 1
-fi
-# send DELETE request using curl and display response body
-curl -X DELETE -i "$1"
+# sends DELETE req to $1 URL and display response body
+curl -s "$1" -X DELETE
